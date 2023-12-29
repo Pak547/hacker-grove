39ac50c0a51b4c689e5e1ff6b24cf685b87647d0
Pak <pakapongm@gmail.com>
2023-12-28 18:39:50 -0800

    wrote post logic for user signup route, interactable with DB and catches error

--- controller/api/user.js
+++ controller/api/user.js
@@ -2,6 +2,35 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-// mails to the user if they forgot their password
 require('dotenv').config();
 const auth = require('../utils/auth');
+
+// signing up will enter information into database
+router.post('/signup', (req, res) => {
+  // user information is taken from body
+  let user = req.body;
+  // query to check if user exists
+  query = "SELECT email, password FROM user WHERE email =?"
+  // checks email
+  connection.query(query, [user.email], (results) => {
+      try {
+          if (results.length <= 0) {
+              // all requirements for user database are required and inserted
+              query = "insert into user (name, email, password) values(?,?,?)"
+              connection.query(query, [user.name, user.contact_number, user.email, user.password], (err) => {
+                  try {
+                      return res.status(200).json({ message: "Successfully Registered!" });
+                  } catch {
+                      return res.status(500).json(err);
+                  }
+              })
+          }
+          else {
+              return res.status(400).json({ message: "Email Already Exists" })
+          }
+      }
+      catch (err) {
+          return res.status(500).json(err);
+      }
+  })
+});
