a0c83b741eab1b7ff282ab3aa4b90dec7f072a33
Pak <pakapongm@gmail.com>
2024-01-03 21:58:10 -0800

    forgotpassword refactoring for nodemailer

--- controller/api/forgotpasswordRoute.js
+++ controller/api/forgotpasswordRoute.js
@@ -2,46 +2,45 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 require('dotenv').config();
-
 // syntax for nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: ['gmail', 'yahoo'],
   auth: {
       user: process.env.EMAIL,
       pass: process.env.EMAIL_PASSWORD
   }
 });
+
 // forgot password will send password to user email if user exists in database 
-router.post('/forgotpassword', (req, res) => {
-  const user = req.body;
-  query = "select email, password from user where email =?";
-  connection.query(query, [user.email], (err, results) => {
-      if (!err) {
-          if (results.length <= 0) {
-              return res.status(200).json({ message: "Password sent to email" });
-          }
-          else {
-              // email information is taken from body and sent to user email
-              const mailOptions = {
-                  from: process.env.EMAIL,
-                  to: results[0].email,
-                  subject: 'Password Reset',
-                  text: 'Your email is' + results[0].email + 'Your password is ' + results[0].password
-              };
-              // send email to user with password information  
-              transporter.sendMail(mailOptions, function (error, info) {
-                  if (error) {
-                      console.log(error)
-                  }
-                  else {
-                      console.log('Email sent: ' + info.response);
-                  }
-              });
-              return res.status(200).json({ message: "Password sent to email" });
-          }
-      }
-      else {
-          return res.status(500).json(err);
-      }
-  })
-});
+router.post('/forgotpassword', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Error' });
+      return;
+    }
+    else {
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: req.body.email,
+        subject: 'Password Reset',
+        text: 'Your email is' + req.body.email + 'Your password is ' + userData.password
+      };
+      // send email to user with password information  
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error)
+        }
+        else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      return res.status(200).json({ message: "Password sent to email" });
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+}
+);
