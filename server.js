cd4cc79931cd4b47767f95a9aeadcfffe0a06504
estan <elise.stankus@gmail.com>
2024-01-04 21:12:14 -0800

    added sequelize, path, routes, session to server

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
-const port = process.env.PORT || 3001
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.use(cors())
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({ helpers });
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`)
-})
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
