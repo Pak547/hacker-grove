c2dde3f7416501ff2550d3de76f92c14c612cdb2
bryancz1493 <bryancz1493@gmail.com>
2024-01-04 19:48:36 -0800

    Modify POST route and add PUT route

--- controller/api/projectRoutes.js
+++ controller/api/projectRoutes.js
@@ -4,79 +4,88 @@ const { User, Project, Language, Tracking } = require('../../models');
 
 // GET all user
 router.get('/', async (req, res) => {
-  try {
-    const projectData = await User.findAll({
-      include: [{ model: user }, { model: Language }, { model: Tracking }],
-    });
-    res.status(200).json(projectData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const projectData = await User.findAll({
+            include: [{ model: user }, { model: Language }, { model: Tracking }],
+        });
+        res.status(200).json(projectData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // GET one user
 router.get('/:id', async (req, res) => {
     try {
-      const userData = await User.findByPk(req.params.id, {
-        include: [{ model: user }, { model: Language }, { model: Tracking }],
-      });
-  
-      if (!projectData) {
-        res.status(404).json({ message: 'No project found with that id!' });
+        const userData = await User.findByPk(req.params.id, {
+            include: [{ model: user }, { model: Language }, { model: Tracking }],
+        });
+
+        if (!projectData) {
+            res.status(404).json({ message: 'No project found with that id!' });
+            return;
+        }
+
+        res.status(200).json(projectData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// POST route to upload data to db
+router.post('/', async (req, res) => {
+    try {
+        const projectData = await Project.create({
+            project_id: req.body.project_id,
+            title: req.body.title,
+            description: req.body.description,
+            hours: req.body.hours,
+            languages: req.body.languages,
+            deploy_link: req.body.deploy_link,
+            github_link: req.body.github_link,
+        });
+        res.status(200).json(projectData);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+// PUT route to update/replace existing data from db
+router.put('/:id', async (req, res) => {
+    // update a project by its id value
+    try {
+      const projectData = await Category.update(
+        {
+          project_name: req.body.project_name,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+      if(!projectData) {
+        res.status(404).json({ message: 'No project found with that id!'});
         return;
       }
-  
       res.status(200).json(projectData);
     } catch (err) {
-      res.status(500).json(err);
+      res.status(400).json(err);
     }
   });
 
-// POST route to upload data to db
-router.post('/', (req, res) => {
-    Project.create({
-      project_id: req.body.project_id,
-      name: req.body.project_name,
-      title: req.body.title,
-      description: req.body.description,
-      hours: req.body.hours,
-      languages: req.body.languages,
-      deploy_link: req.body.deploy_link,
-      github_link: req.body.github_link,
-    })
-      .then((project) => {
-        if (req.body.tagIds.length) {
-          const productTagIdArr = req.body.tagIds.map((tag_id) => {
-            return {
-              product_id: product.id,
-              tag_id,
-            };
-          });
-          return ProductTag.bulkCreate(productTagIdArr);
-        }
-        res.status(200).json(product);
-      })
-      .then((productTagIds) => res.status(200).json(productTagIds))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  });
-
-  // PUT route to update/replace existing data from db
-
-  // DELETE route to remove existing data from db
-  router.delete('/:id', (req, res) => {
+// DELETE route to remove existing data from db
+router.delete('/:id', (req, res) => {
     // delete one product by its `id` value
-    Project.destroy ({
-      where: {
-        id: req.params.id,
-      },
+    Project.destroy({
+        where: {
+            id: req.params.id,
+        },
     })
-      .then((deletedProject) => {
-        res.json(deletedProject);
-      })
-      .catch((err) => res.json(err));
-  });
+        .then((deletedProject) => {
+            res.json(deletedProject);
+        })
+        .catch((err) => res.json(err));
+});
 
 module.exports = router;
