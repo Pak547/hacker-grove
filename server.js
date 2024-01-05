310976a9ae0648cb47db11595e1e97ac5bb57ba7
estan <elise.stankus@gmail.com>
2024-01-04 21:03:48 -0800

    add handlebars to server

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
