c08010d87ff8f2950a82af12dd53fdb46552ba35
Pak <pakapongm@gmail.com>
2024-01-05 17:39:57 -0800

    currently working on homeRoute configuration, allows login and signup as well as renders homepage, new bug currently w views

--- controller/api/languageRoutes.js
+++ controller/api/languageRoutes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
+const Language = require('../../models/Language');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const languageData = await Language.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['name', 'ASC']],
+        });
+        const languages = languageData.map((project) => project.get({ plain: true }));
+        res.render('homepage', {
+            languages,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 
 module.exports = router;
--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,38 +1,31 @@
 const router = require('express').Router();
-const User = require('../models/User');
-const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-    const users = userData.map((project) => project.get({ plain: true }));
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-  res.render('login');
-});
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
 
-router.get('/signup', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-  res.render('signup');
-});
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
 
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('signup');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
 
 module.exports = router;
