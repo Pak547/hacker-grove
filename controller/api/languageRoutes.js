1c7df55dbdc1a66dac8a10305966fa2048f8fa61
Pak <pakapongm@gmail.com>
2024-01-05 18:29:56 -0800

    finished language route and synced to models

--- controller/api/languageRoutes.js
+++ controller/api/languageRoutes.js
@@ -2,18 +2,27 @@ const router = require('express').Router();
 const Language = require('../../models/Language');
 const withAuth = require('../../utils/auth');
 
-
+// GET all language
 router.get('/', async (req, res) => {
     try {
-        const languageData = await Language.findAll({
-            attributes: { exclude: ['password'] },
-            order: [['name', 'ASC']],
-        });
-        const languages = languageData.map((project) => project.get({ plain: true }));
-        res.render('homepage', {
-            languages,
-            logged_in: req.session.logged_in,
-        });
+        const languageData = await Language.findAll();
+        res.status(200).json(languageData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET one language by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const languageData = await Language.findByPk(req.params.id);
+
+        if (!languageData) {
+            res.status(404).json({ message: 'No language found with that id!' });
+            return;
+        }
+
+        res.status(200).json(languageData);
     } catch (err) {
         res.status(500).json(err);
     }
--- controller/api/trackerRoutes.js
+++ controller/api/trackerRoutes.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const Tracking = require('../../models/Tracking');
 const withAuth = require('../../utils/auth');
 
-module.export = router;
+
+// GET all trackings for a user
