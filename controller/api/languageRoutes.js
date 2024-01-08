a8c0cbe85a9a9388607f953dba738b7b2969c514
Pak <pakapongm@gmail.com>
2024-01-08 12:43:38 -0800

    added delete path if they want to delete language

--- controller/api/languageRoutes.js
+++ controller/api/languageRoutes.js
@@ -28,7 +28,6 @@ router.post('/', withAuth, async (req, res) => {
   });
 
 router.put('/:id', async (req, res) => {
-    // update a language by its id value
     try {
         const languageData = await Language.update(
             {
@@ -50,6 +49,25 @@ router.put('/:id', async (req, res) => {
     }
 }
 );
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const languageData = await Language.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!languageData) {
+            res.status(404).json({ message: 'No language found with that id!' });
+            return;
+        }
+        res.status(200).json(languageData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
 
   
 
