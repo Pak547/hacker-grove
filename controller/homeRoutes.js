4936746328b0c0ca2779cdf5d65787c649d9ee7a
Pak <pakapongm@gmail.com>
2024-01-08 12:16:40 -0800

    added proper config for attributes for models in get routes for each

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Project, User, Language } = require('../models');
+const { Project, User, Language, Tracking } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -72,6 +72,7 @@ router.get('/user', withAuth, async (req, res) => {
                         'languages',
                         'deploy_link',
                         'github_link',
+                        'user_id'
                     ],
                 },
                 {
@@ -80,14 +81,39 @@ router.get('/user', withAuth, async (req, res) => {
                         'id',
                         'hours',
                         'language',
+                        'user_id'
                     ],
                 },
+                // future work
+                {
+                    model: Tracking,
+                    attributes: [
+                        'id',
+                        'hours',
+                        'user_id',
+                    ],
+                }
             ],
         });
         const user = userData.get({ plain: true });
         res.render('user', { user, loggedIn: true });
     } catch (err) {
         console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', withAuth, async (req, res) => {
+    try {
+        const languageData = await Language.findByPk(req.params.id);
+
+        if (!languageData) {
+            res.status(404).json({ message: 'We could not find that language!' });
+            return;
+        }
+
+        res.status(200).json(languageData);
+    } catch (err) {
         res.status(500).json(err);
     }
 });
