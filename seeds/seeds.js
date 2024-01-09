ded0468c4f5d5eac4c90f8e0820b23607829bf44
Pak <pakapongm@gmail.com>
2024-01-08 17:39:37 -0800

    added test for users for project and for language in seeds.js

---
+++ seeds/seeds.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { User, Project, Language } = require('../models');
+
+const userData = require('./userData.json');
+const projectData = require('./projectData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const project of projectData) {
+    await Project.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const language of languageData) {
+    await Language.create({
+      ...language,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+
+seedDatabase();
