29b7239ade74cce9cd2be4280fdba37202e502de
estan <elise.stankus@gmail.com>
2024-01-10 11:41:08 -0800

    commented out all code

--- models/Tracking.js
+++ models/Tracking.js
@@ -1,36 +1,36 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-class Tracking extends Model { }
+// class Tracking extends Model { }
 
-Tracking.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    total_hours: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tracking',
-  }
-);
+// Tracking.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     total_hours: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       defaultValue: 0,
+//     },
+//     user_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: 'user',
+//         key: 'id',
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'tracking',
+//   }
+// );
 
-module.exports = Tracking;
+// module.exports = Tracking;
